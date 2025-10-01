9d219104079cf810f5496f5b26335aa0259b0c05
Mayank Kumar <mayankcicia@gmail.com>
2025-10-01 13:48:33 +0530

    Add Web Controller for index

---
+++ src/main/java/com/project1/To/Do/List/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.project1.To.Do.List.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "index";
+    }
+}
